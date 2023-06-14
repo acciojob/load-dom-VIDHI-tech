a15abb1c7b3c8ee8b285833a3b8ed85a7021255f
Vidhi Gaydhane <120549998+VIDHI-tech@users.noreply.github.com>
2023-06-14 19:19:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function () {
+	// Select the body element
+	var body = document.querySelector("body");
+
+	// Clear any existing content inside the body
+	body.innerHTML = "";
+
+	// Create a text node with the string "DOM load success"
+	var textNode = document.createTextNode("DOM load success");
+
+	// Append the text node to the body
+	body.appendChild(textNode);
+});
